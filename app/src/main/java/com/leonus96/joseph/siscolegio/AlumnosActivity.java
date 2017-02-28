9df2b42c9b4f5d3cdbf6945bdcc6f409a54acda7
Joseph León <leonus96@gmail.com>
2017-02-28 16:28:30 -0500

    Estructura armada

--- app/src/main/java/com/leonus96/joseph/siscolegio/AlumnosActivity.java
+++ app/src/main/java/com/leonus96/joseph/siscolegio/AlumnosActivity.java
@@ -1,13 +1,19 @@
 package com.leonus96.joseph.siscolegio;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class AlumnosActivity extends AppCompatActivity {
+
+    @BindView(R.id.card1) CardView _nino;
+    @BindView(R.id.card2) CardView _nina;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +22,29 @@ public class AlumnosActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Selecciona un alumno");
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
+        final Intent intent = new Intent(this, AgendaActivity.class);
+        final Bundle bundle = new Bundle();
 
+        _nino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bundle.putInt("id", 1);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
+        _nina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bundle.putInt("id", 2);
+                intent.putExtras(bundle);
+                startActivity(intent);
+
+            }
+        });
     }
 
 }
